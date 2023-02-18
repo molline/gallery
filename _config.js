71bd92a2d41ec5dcff44174dcc0af50eeaf91f25
molline <molline35@gmail.com>
2023-02-18 23:29:03 +0300

    milestone 2

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Molline:sviKotCg5uWH2EXo@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Molline:sviKotCg5uWH2EXo@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Molline:sviKotCg5uWH2EXo@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    // production: 'mongodb+srv://Molline:sviKotCg5uWH2EXo@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    // development: 'mongodb+srv://Molline:sviKotCg5uWH2EXo@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    // test: 'mongodb+srv://Molline:sviKotCg5uWH2EXo@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production:'mongodb+srv://Molline:molly123@cluster0.jgdnqqi.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development:'mongodb+srv://Molline:molly123@cluster0.jgdnqqi.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test:'mongodb+srv://Molline:molly123@cluster0.jgdnqqi.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
