ce28f091e4c38cf6ec90cac45ce50a07031843a8
molline <molline35@gmail.com>
2023-02-19 14:37:52 +0300

    test merged to master

--- server.js
+++ server.js
@@ -2,33 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./_config');
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
+// // connecting the database
+async function connectdb() {
+    await mongoose
+      .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connected to MongoDB");
+      })
+      .catch((err) => {
+        console.log("Error connecting to MongoDB", err.message);
+      });
+  }
+
 // Initializing the app
 const app = express();
-
-// connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
 
 
 // View Engine
@@ -48,9 +45,8 @@ app.use('/image', image);
 
  
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
+app.listen(PORT, async () =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
+    await connectdb();
 });
-
-
 module.exports = app;
